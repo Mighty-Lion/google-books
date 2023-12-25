60e8d465a45c3126b5ffe29943f62acffffc4be6
Aslan Abaev <hedgehog5349@gmail.com>
2023-12-25 16:36:14 +0300

    dev axios request

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,53 +9,36 @@ import {
 } from '@/components/BooksSection/index.styles';
 import { BookCard } from '@/components/BooksSection/partials/BookCard';
 import SearchIcon from '@/assets/images/svg/search.svg';
+import { LoadingSpinner } from '@/components/LoadingSpiner';
 
 export default function Home() {
+  const toastNotifications = useToastNotifications();
   const { values, handleInput, handleSubmit } = useHandleInput();
   const apiUrl = 'https://www.googleapis.com/books/v1/volumes';
   const apiKey = 'AIzaSyBWdD2QpIiQ_AbBmwLNeBHSTE2rY1zu-Uw';
   const { input, sorting, category } = values;
-  const categoryParams = category !== 'all' ? `+subject:${category}` : '';
-  const searchParams = input || 'java script';
-  const inputParams = searchParams + categoryParams;
-  // console.log('input', input);
-  // console.log('inputParams', inputParams);
 
-  // const savedPosts = localStorage.getItem('savedPosts');
-  // const parsedPosts = savedPosts && JSON.parse(savedPosts);
-  const toastNotifications = useToastNotifications();
-
-  // const [posts, setPosts] = useState<IPostProps[]>(parsedPosts || []);
   const [books, setBooks] = useState<any>([]);
   const [isFetching, setIsFetching] = useState(false);
 
-  const testT = {
-    searchTerm: '',
-    printType: 'all',
-    bookType: 'full',
+  const params = {
+    q: `${input || 'java script'}:${
+      category === 'all' ? '' : `+subject:${category}`
+    }`,
+    orderBy: `${sorting}`,
+    key: `${apiKey}`,
   };
 
   async function fetchData() {
     try {
-      // console.log('values', values);
-
       setIsFetching(true);
-      const response = await axios.get(apiUrl, {
-        params: {
-          q: `${searchParams}`,
-          language: 'ru',
-          orderBy: `${sorting}`,
-          key: `${apiKey}`,
-        },
-      });
+      const response = await axios.get(apiUrl, { params });
       setBooks(response.data);
-      // console.log('response', response.config.params);
     } catch (error) {
       let errorMessage = 'Failed to do something exceptional';
       if (error instanceof Error) {
         errorMessage = error.message;
       }
-      console.log('handelSendReminder', errorMessage);
       toastNotifications.handleFailure(errorMessage);
     } finally {
       setIsFetching(false);
@@ -66,31 +49,41 @@ export default function Home() {
     fetchData();
   }, [values]);
 
-  console.log('books',books.items)
-  const mappedBooks = books.items ?  books.items?.map((item: any) => {
-        console.log(`item.volumeInfo.authors ${item.volumeInfo.authors}`)
-        const author = item.volumeInfo.authors ? item.volumeInfo.authors.reduce(
-            (accumulator: any, currentValue: any) => accumulator + currentValue + '\n',
-            '',
-        ) : '';
-        console.log(`author ${author}`)
-        return <BookCard
-            key={item.volumeInfo.title}
-            img={item.volumeInfo.imageLinks !== undefined ? item.volumeInfo.imageLinks.smallThumbnail : SearchIcon}
-            category={item.volumeInfo.categories}
-            name={item.volumeInfo.title}
-            author={author}
+  console.log('books', books.items);
+  const mappedBooks = books.items ? (
+    books.items?.map((item: any) => {
+      const author = item.volumeInfo.authors
+        ? item.volumeInfo.authors.reduce(
+            (accumulator: any, currentValue: any) =>
+              `${accumulator + currentValue}\n`,
+            ''
+          )
+        : '';
+      return (
+        <BookCard
+          key={item.volumeInfo.title}
+          img={
+            item.volumeInfo.imageLinks !== undefined
+              ? item.volumeInfo.imageLinks.smallThumbnail
+              : SearchIcon
+          }
+          category={item.volumeInfo.categories}
+          name={item.volumeInfo.title}
+          author={author}
         />
-      }
-       ) : <div>Loading...</div>
-
-
+      );
+    })
+  ) : (
+    <div>Loading...</div>
+  );
 
   return (
     <>
       <SearchSection handleInput={handleInput} handleSubmit={handleSubmit} />
       <BooksSectionWrapper>
-        <BooksSectionContainer>{mappedBooks}</BooksSectionContainer>
+        <BooksSectionContainer>
+          {isFetching ? <LoadingSpinner /> : mappedBooks}
+        </BooksSectionContainer>
       </BooksSectionWrapper>
     </>
   );
