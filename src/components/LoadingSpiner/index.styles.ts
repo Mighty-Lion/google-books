4140e0e41c8cb913d81bedbddf4be961238294f7
Aslan Abaev <hedgehog5349@gmail.com>
2023-12-25 16:34:41 +0300

    created LoadingSpiner

---
+++ src/components/LoadingSpiner/index.styles.ts
@@ -0,0 +1,24 @@
+import { keyframes } from '@emotion/css';
+import styled from '@emotion/styled';
+
+const spinner = keyframes`
+        0%  {
+              transform: rotate(0deg);
+        }
+         100% {
+              transform: rotate(360deg);
+        }
+    `;
+
+export const SpinnerContainer = styled.div`
+  margin: 0 auto;
+`;
+
+export const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 10px solid #f3f3f3; /* Light grey */
+  border-top: 10px solid #383636; /* Black */
+  border-radius: 50%;
+  animation: spinner 1.5s linear infinite;
+`;
---
+++ src/components/LoadingSpiner/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Spinner,
+  SpinnerContainer,
+} from '@/components/LoadingSpiner/index.styles';
+
+export function LoadingSpinner() {
+  return (
+    <SpinnerContainer>
+      <Spinner />
+    </SpinnerContainer>
+  );
+}
