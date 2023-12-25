42fac22b5980aee546d68f0841133da5146ba34a
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-12-25 20:59:43 +0300

    dev BooksSection

--- src/components/BooksSection/index.tsx
+++ src/components/BooksSection/index.tsx
@@ -1,35 +1,51 @@
+import { useEffect } from 'react';
 import {
   BooksSectionContainer,
   BooksSectionWrapper,
 } from '@/components/BooksSection/index.styles';
 import { BookCard } from '@/components/BooksSection/partials/BookCard';
-import {useEffect} from "react";
+import { LoadingSpinner } from '@/components/LoadingSpiner';
+import SearchIcon from '@/assets/images/svg/search.svg';
 
 export interface IBooksSectionProps {
   books: any;
+  isFetching: boolean;
 }
-export function BooksSection({ books }: IBooksSectionProps) {
-
-  /* eslint-disable */
-  // @ts-ignore
-
-  const imgSrc = item.volumeInfo.imageLinks.smallThumbnail;
-  console.log('imgSrc', imgSrc)
-  const mappedBooks = books['items']?.map((item: any) => (
-    /* eslint-enable */
-    <BookCard
-
-      img={imgSrc}
-      category={item.volumeInfo.categories}
-      name={item.volumeInfo.title}
-      author={item.volumeInfo.authors}
-    />
-  ));
-
+export function BooksSection({ isFetching, books }: IBooksSectionProps) {
+  const mappedBooks = books.items ? (
+    books.items.map((item: any) => {
+      const imgSrc =
+        item.volumeInfo.imageLinks !== undefined
+          ? item.volumeInfo.imageLinks.smallThumbnail
+          : SearchIcon;
+      const author = item.volumeInfo.authors
+        ? item.volumeInfo.authors.reduce(
+            (accumulator: any, currentValue: any) =>
+              `${accumulator + currentValue}\n`,
+            ''
+          )
+        : '';
+      return (
+        <BookCard
+          key={
+            item.volumeInfo.title + item.volumeInfo.categories + author + imgSrc
+          }
+          img={imgSrc}
+          category={item.volumeInfo.categories}
+          name={item.volumeInfo.title}
+          author={author}
+        />
+      );
+    })
+  ) : (
+    <div>Loading...</div>
+  );
 
   return (
     <BooksSectionWrapper>
-      <BooksSectionContainer>{mappedBooks}</BooksSectionContainer>
+      <BooksSectionContainer>
+        {isFetching ? <LoadingSpinner /> : mappedBooks}
+      </BooksSectionContainer>
     </BooksSectionWrapper>
   );
 }
