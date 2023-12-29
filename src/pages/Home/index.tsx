5a6b9811dae5c9d748439fb49195f40802db543a
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-12-30 01:11:41 +0300

    added logic for displaying details about the book

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,29 +4,13 @@ import { useFormData } from '@/hooks/useFormData';
 import { IBookProps, useFetchData } from '@/hooks/useFetchData';
 import { BooksSection } from '@/components/BooksSection';
 import { BookDetails } from '@/components/BookDetails';
+import { useSelectedBook } from '@/hooks/useSelectedBook';
 
 export default function Home() {
   const { values, handleInput, handleSubmit } = useFormData();
   const { data, books, isFetching, handleUpdate } = useFetchData(values);
-
-  const [selectedBookId, setSelectedBookId] = useState<string | undefined>(
-    undefined
-  );
-  const [selectedBook, setSelectedBook] = useState<IBookProps | undefined>(
-    undefined
-  );
-
-  function selectBook() {
-    function callback(element: IBookProps) {
-      return element.id === selectedBookId && element;
-    }
-
-    return books.find(callback);
-  }
-
-  useEffect(() => {
-    setSelectedBook(selectBook());
-  }, [selectedBookId]);
+  const { selectedBookId, setSelectedBookId, selectedBook } =
+    useSelectedBook(books);
 
   return (
     <>
