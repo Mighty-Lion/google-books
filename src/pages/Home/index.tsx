88f25f6e4e5a36ceac71d33acd666794ceda15de
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-12-30 00:45:40 +0300

    dev select book logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,38 +3,49 @@ import { SearchSection } from '@/components/SearchSection';
 import { useFormData } from '@/hooks/useFormData';
 import { IBookProps, useFetchData } from '@/hooks/useFetchData';
 import { BooksSection } from '@/components/BooksSection';
+import { BookDetails } from '@/components/BookDetails';
 
 export default function Home() {
   const { values, handleInput, handleSubmit } = useFormData();
   const { data, books, isFetching, handleUpdate } = useFetchData(values);
 
-  const [bookId, setBookId] = useState<string | undefined>(undefined);
+  const [selectedBookId, setSelectedBookId] = useState<string | undefined>(
+    undefined
+  );
+  const [selectedBook, setSelectedBook] = useState<IBookProps | undefined>(
+    undefined
+  );
 
-  console.log('bookId click', bookId);
-
-  function findBookId() {
+  function selectBook() {
     function callback(element: IBookProps) {
-      return element.id === bookId && element;
+      return element.id === selectedBookId && element;
     }
 
     return books.find(callback);
   }
 
   useEffect(() => {
-    findBookId();
-  }, [bookId]);
+    setSelectedBook(selectBook());
+  }, [selectedBookId]);
 
   return (
     <>
       <SearchSection handleChange={handleInput} handleSubmit={handleSubmit} />
-      <BooksSection
-        key={data?.items[0]?.volumeInfo?.title}
-        data={data}
-        books={books}
-        isFetching={isFetching}
-        handleUpdate={handleUpdate}
-        setBookId={setBookId}
-      />
+      {selectedBookId === undefined ? (
+        <BooksSection
+          key={data?.items[0]?.volumeInfo?.title}
+          data={data}
+          books={books}
+          isFetching={isFetching}
+          handleUpdate={handleUpdate}
+          setSelectedBookId={setSelectedBookId}
+        />
+      ) : (
+        <BookDetails
+          selectedBook={selectedBook}
+          setSelectedBookId={setSelectedBookId}
+        />
+      )}
     </>
   );
 }
