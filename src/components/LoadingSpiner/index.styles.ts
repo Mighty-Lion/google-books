de92d8590b0d8081da54f1a889397eb32222bff7
Aslan Abaev <hedgehog.asl@yandex.ru>
2024-01-05 19:10:12 +0300

    fix LoadingSpiner

--- src/components/LoadingSpiner/index.styles.ts
+++ src/components/LoadingSpiner/index.styles.ts
@@ -20,5 +20,5 @@ export const Spinner = styled.div`
   border: 10px solid #f3f3f3; /* Light grey */
   border-top: 10px solid #383636; /* Black */
   border-radius: 50%;
-  animation: spinner 1.5s linear infinite;
+  animation: ${spinner} 1.5s linear infinite;
 `;
